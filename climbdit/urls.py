15ef18f5eb627e6e3166ca01c917a1bc4e78729f
Nitesh <nitlmisra@hotmail.com>
2022-01-30 04:27:45 -0500

    fix urls

--- climbdit/urls.py
+++ climbdit/urls.py
@@ -1,12 +1,2 @@
-from rest_framework import routers
+# from rest_framework import routers
 
-from climbdit.views import StateViewSet, LocationViewSet, ClimberViewSet, ClimbViewSet
-
-
-router = routers.DefaultRouter()
-router.register('state', StateViewSet)
-router.register('location', LocationViewSet)
-router.register('climber', ClimberViewSet)
-router.register('climb', ClimbViewSet)
-
-urlpatterns = router.urls
--- config/urls.py
+++ config/urls.py
@@ -17,10 +17,20 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-# from rest_framework import routers
+from rest_framework import routers
 # from climbdit.views import StateViewSet, LocationViewSet, ClimberViewSet, ClimbViewSet
 
 
+from climbdit.views import StateViewSet, LocationViewSet, ClimberViewSet, ClimbViewSet
+
+
+router = routers.DefaultRouter()
+router.register('state', StateViewSet)
+router.register('location', LocationViewSet)
+router.register('climber', ClimberViewSet)
+router.register('climb', ClimbViewSet)
+
+# urlpatterns = router.urls
 
 urlpatterns = [
     path('', include(router.urls)),
